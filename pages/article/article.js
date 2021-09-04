2a66d71d76c769d0b6d7689dfa0989f5346e42af
itechnica <83644626+itechnica@users.noreply.github.com>
2021-09-04 14:35:04 +0800

    Remove unused imports (and references to them)

--- pages/article/article.js
+++ pages/article/article.js
@@ -1,5 +1,4 @@
 import { useRouter } from "next/router";
-import { useEffect } from 'react'
 import Head from 'next/head'
 import Header from '@components/Header'
 import Footer from '@components/Footer'
@@ -8,10 +7,6 @@ export default function Article(props) {
 
   const {query} = useRouter();
   const history_url = "/banana/77/an-article-title"
-  const router = useRouter();
-
-  router.replace(history_url);
-
 
   return (
     <div className="container">
